Minor fixes. Added data normalization script.

config.py
@@ -1,43 +1,20 @@
 # DATABASE
 db_path='db/zenodo.org/record/1117372/files/'
+log_dir = 'stft/log/'
 
+# Algorithm regulariation values
 alpha = 0.001
 beta  = 0.01
 beta_voc = 0.03
 
-channels = 2
-features = 513
-num_epochs = 50
-batches_per_epoch_train = 2
-
-max_len = 3939892
-channels = 2
-features = 513
-
-split = 0.9
-
 # Hyperparameters
-num_epochs = 8000
-batches_per_epoch_train = 50
-batches_per_epoch_val = 50
-batch_size = 5
-samples_per_file = 1
-max_phr_len = 30
 input_features = 513
-lstm_size = 128
 output_features = 66
 highway_layers = 4
 highway_units = 128
-init_lr = 1
 num_conv_layers = 8
 conv_filters = 128
-num_ch_out_hor = 50
-num_ch_out_ver = 30
+
 # conv_activation = tf.nn.relu
-dropout_rate = 0.0
-projection_size = 3
 fs = 44100
 comp_mode = 'mfsc'
-dn_num_epochs = 4000
-print_every = 1
-save_every = 100

dataPrep.py
@@ -26,7 +26,7 @@ def prepData(filenames):
         hdf5_file["tar_stft"][:,:,:] = np.concatenate((voice, drums, bass, comp),axis = 0)
         hdf5_file.close()
 
-def normalizeData():
+def normalizeDataPrep():
     print('Normalizing data')
     maximus = np.zeros((10,1,513))
     minimus = np.zeros((10,1,513))
@@ -57,6 +57,33 @@ def normalizeData():
     hdf5_file["feats_minimus"][:] = minimus.reshape(10,513)
     hdf5_file.close()
 
+def normalizeData():
+
+    file_list = [x for x in os.listdir('stft/train/') if x.endswith('.hdf5') and not x.startswith('stats')]
+    
+    stat_file = h5py.File('stft/train/stats.hdf5', mode='r')
+    
+    max_feat = np.array(stat_file["feats_maximus"])
+    min_feat = np.array(stat_file["feats_minimus"])
+
+    max_feat_tars = max_feat[:8,:].reshape(1,8,1,513)
+    min_feat_tars = min_feat[:8,:].reshape(1,8,1,513)
+
+    max_feat_ins = max_feat[-2:,:].reshape(1,2,1,513)
+    min_feat_ins = min_feat[-2:,:].reshape(1,2,1,513)
+
+    for fi in file_list:
+        print('Normalizing file {}'.format(fi))
+        hdf5_file = h5py.File('stft/train/'+fi, "a")
+        tar_stft = hdf5_file["tar_stft"]
+        mix_stft = hdf5_file['mix_stft']
+
+        norm_tar = (np.array(tar_stft) - min_feat_tars)/(max_feat_tars - min_feat_tars)
+        norm_mix = (np.array(mix_stft) - min_feat_ins)/(max_feat_ins - min_feat_ins) 
+        hdf5_file["mix_stft"][:,:,:] = norm_mix
+        hdf5_file["tar_stft"][:,:,:] = norm_tar
+        hdf5_file.close()
+
 
 def listBucketFiles(bucket):
     
@@ -77,11 +104,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process dataset from bucket')
     parser.add_argument('-db','--database', type=str,
                         help='Optional: argument to indicate optional bucket name. Default: musdb')
-    parser.add_argument('-m','--maxmin', type=str,
+    parser.add_argument('-m','--maxmin', action="store_true",
                         help='Optional: Compute max and mins across dataset')
+    parser.add_argument('-n', '--normalize', action="store_true",
+                        help='Optional: normalize dataset')
     args = parser.parse_args()
-    if args.maxmin:
-        normalizeData()
+    
     if args.database:
         bucket_name = args.database
     else:
@@ -92,6 +120,11 @@ if __name__ == '__main__':
         bucket = client.get_bucket(bucket_name)   
     except:
         print('Can\'t find/access bucket')
-
-    file_list = listBucketFiles(bucket)
-    prepData(file_list)
+    
+    if args.maxmin:
+        normalizeDataPrep()
+    elif args.normalize:
+        normalizeData()
+    else:
+        file_list = listBucketFiles(bucket)
+        prepData(file_list)

main.py
@@ -1,5 +1,6 @@
 from __future__ import print_function, division
 import datetime,time
+import argparse
 
 import numpy as np
 import torch
@@ -151,7 +152,7 @@ def loss_calc(inputs, targets, loss_func, autoencoder):
 
 
 
-def trainNet():
+def trainNet(file=None):
     autoencoder = AutoEncoder().cuda()
     optimizer = torch.optim.Adadelta(autoencoder.parameters(), lr = 1, rho=0.95)
     loss_func = nn.MSELoss( size_average=False )
@@ -189,7 +190,7 @@ def trainNet():
         
         if (epoch % SAVE_EVERY == 0):
             now = datetime.datetime.now()
-            model_name = 'log/{}{}{}{}_{}.pt'.format(now.month, now.day, now.hour, now.minute, epoch) 
+            model_name = config.log_dir+'{}{}{}{}_{}.pt'.format(now.month, now.day, now.hour, now.minute, epoch) 
             torch.save(autoencoder.state_dict(), model_name)
             print('Model saved in {}'.format(model_name))
 
@@ -206,6 +207,17 @@ def trainNet():
 
 
 if __name__ == '__main__':
-    print('Training network')
-    trainNet()
+    parser = argparse.ArgumentParser(description='Main CloudSep run file')
+    parser.add_argument('-t','--train', action='store_true',
+                        help='Indicate to start training network')
+    parser.add_argument('-f','--file', type=str,
+                        help='Optional: load a specific trained model into the network')
+    args = parser.parse_args()
+    if args.train:
+        print('Training network')
+        if args.file:
+            trainNet(file=args.file)
+        else:
+            trainNet()
+
     #print (torch.cuda.is_available())

pipeline.py
@@ -12,11 +12,11 @@ MAX_TIME_CONTEXT=30
 
 def dataGen():
     
-    filenames = [x for x in os.listdir('stft/train/') if x.endswith('.hdf5')]
+    filenames = [x for x in os.listdir('stft/train/') if x.endswith('.hdf5') and not x.startswith('stats')]
     num_files = len(filenames)
     for i in range(NUM_EPOCH):
-        inputs = []
         targets = []
+        inputs = []
         for i in range(FILES_PER_BATCH):
             rand_file = filenames[random.randint(0,num_files-1)]
             hdf5_file = h5py.File('stft/train/'+rand_file, "r+")
@@ -24,11 +24,11 @@ def dataGen():
             mix_stft = hdf5_file['mix_stft']
             for i in range(SAMPLES_PER_FILE):
                 rand_index = np.random.randint(0,mix_stft.shape[1]-MAX_TIME_CONTEXT)
-                inputs.append(tar_stft[:,rand_index:rand_index+MAX_TIME_CONTEXT,:])
-                targets.append(mix_stft[:,rand_index:rand_index+MAX_TIME_CONTEXT,:])
+                targets.append(tar_stft[:,rand_index:rand_index+MAX_TIME_CONTEXT,:])
+                inputs.append(mix_stft[:,rand_index:rand_index+MAX_TIME_CONTEXT,:])
             hdf5_file.close()
             
-        yield np.array(targets), np.array(inputs)
+        yield np.array(inputs), np.array(targets)
 
 if __name__ == '__main__':
     gen = dataGen()